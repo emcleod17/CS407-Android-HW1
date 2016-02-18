2570eadc9f2b093f914e28fac94e3a61c0040b32
emcleod17 <emcleod@wisc.edu>
2016-02-18 14:05:33 -0600

    -Win activity now exists, not yet implemented. Working on implementing QuizFragment2 to go to Win activity.

--- app/src/main/java/com/example/emcleod/cs407_android_hw1/QuizFragment2.java
+++ app/src/main/java/com/example/emcleod/cs407_android_hw1/QuizFragment2.java
@@ -5,11 +5,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by emcleod on 2/18/16.
  */
 public class QuizFragment2 extends Fragment{
+
+    private Button wow_radioButton;
 
     public QuizFragment2() {
         // Required empty public constructor
@@ -21,6 +24,25 @@ public class QuizFragment2 extends Fragment{
         View view = null;
         view = inflater.inflate(R.layout.fragment_quizfragment2, container, false);
 
+        wow_radioButton = (Button) view.findViewById(R.id.wow_radioButton);
+
         return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        wow_radioButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                choicePressed(wow_radioButton);
+            }
+
+        });
+    }
+
+    public void choicePressed(View view) {
+
     }
 }
---
+++ app/src/main/java/com/example/emcleod/cs407_android_hw1/WinActivity.java
@@ -0,0 +1,8 @@
+package com.example.emcleod.cs407_android_hw1;
+
+/**
+ * Created by emcleod on 2/18/16.
+ */
+public class WinActivity {
+
+}
