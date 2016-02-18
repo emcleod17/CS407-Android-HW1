20bbb622c4091f7de0d0f5ddf55d3052898275e5
emcleod17 <emcleod@wisc.edu>
2016-02-17 22:20:31 -0600

    -Welcome screen activity implemented, launches quiz activity (not yet implemented)

--- app/src/main/java/com/example/emcleod/cs407_android_hw1/MainActivity.java
+++ app/src/main/java/com/example/emcleod/cs407_android_hw1/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.emcleod.cs407_android_hw1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button start_button = (Button) findViewById(R.id.start_button);
+        start_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                playPressed(start_button);
+            }
+        });
+    }
+
+    public void playPressed(View view) {
+        //TODO: Put to start game play activity
+        Intent intent = new Intent(this, QuizActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/emcleod/cs407_android_hw1/QuizActivity.java
@@ -0,0 +1,16 @@
+package com.example.emcleod.cs407_android_hw1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by emcleod on 2/17/16.
+ */
+public class QuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+    }
+}
